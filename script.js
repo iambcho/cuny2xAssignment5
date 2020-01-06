5ec0ef3f68ff61e66201311975a56618a1598464
Brittany Cho <bac411@nyu.edu>
2020-01-06 14:58:57 -0500

    adding AJ's examples for all files

---
+++ script.js
@@ -0,0 +1,23 @@
+//write a function that will add a row to the grid
+
+//counter that keeps track of amount of rows or columns
+//row will have as many rows areas there are columns
+
+let amountofRows = 1;
+let amountofColumns = 2;
+
+function addRow() {
+    //grab the main grid
+    let mainGrid = document.getElementById("main-grid");
+    //make the row that we want to poplate and append to the table on the DOM
+    let newRow = document.createElement("tr");
+
+    //populate the row with "squares" or cells aka TD elements
+
+    for(let i = 0; i < amountofColumns; i++) {
+        let cell = document.createElement("td");
+        newRow.appendChild(cell);
+    }
+    mainGrid.appendChild(newRow);
+    amountofRows++;
+}
